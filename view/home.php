cd213344342ce7e7b94dc196d51b20fc5cd40ecd
Enzo Jimenez <enzo-jimenez@hotmail.com>
2020-10-31 20:02:07 -0300

    menu en home

--- view/home.php
+++ view/home.php
@@ -1,3 +1,53 @@
 {{> header}}
-    <h1>Home</h1>
+<h1>Bienvenido usuario</h1>
+<h3>Menu</h3>
+<div class="">
+    <div class="col-12 d-flex mb-3">
+        <div class="card col-md-4">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Viajes</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        <div class="card col-md-4">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Empleados</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+        <div class="card col-md-4">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Vehiculos</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 d-md-flex mb-3">
+        <div class="card col-md-4 mb-2">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Reportes</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+        </div>
+        <div class="card col-md-4 mb-2">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Seguimiento</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+        </div>
+        <div class="card col-md-4 mb-2">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Mantenimiento</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
 {{> footer}}
