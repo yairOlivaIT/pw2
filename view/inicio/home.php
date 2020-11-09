445e7d50f2ca62e88c1aabc97080a11370c064b2
Enzo Jimenez <enzo-jimenez@hotmail.com>
2020-11-09 14:41:03 -0300

    iconos en menu

--- view/inicio/home.php
+++ view/inicio/home.php
@@ -1,24 +1,29 @@
 {{> headerInterno}}
-<h1>Bienvenido {{nombre}}</h1>
-<h3>Menu</h3>
+<h2>Bienvenido {{nombre}}</h2>
 <div class="contenedor">
     <div class="col-12 d-md-flex">
         <div class="card col-md-4 mb-2">
-            <img src="/public/images/pikachu.jpg" class="card-img-top" alt="...">
+            <div class="m-auto py-3">
+                <i class="fas fa-route icono-menu"></i>
+            </div>
             <div class="card-body">
                 <h5 class="card-title"><a href="/viaje">Viajes</a></h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
         </div>
         <div class="card col-md-4 mb-2">
-            <img src="..." class="card-img-top" alt="...">
+            <div class="m-auto py-3">
+                <i class="fas fa-user-alt icono-menu"></i>
+            </div>
             <div class="card-body">
                 <h5 class="card-title"><a href="/empleado">Empleados</a></h5>
                 <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
             </div>
         </div>
         <div class="card col-md-4 mb-2">
-            <img src="..." class="card-img-top" alt="...">
+            <div class="m-auto py-3">
+                <i class="fas fa-truck-moving icono-menu"></i>
+            </div>
             <div class="card-body">
                 <h5 class="card-title"><a href="/vehiculo">Vehiculos</a></h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -27,27 +32,32 @@
     </div>
     <div class="col-12 d-md-flex">
         <div class="card col-md-4 mb-2">
-            <img src="..." class="card-img-top" alt="...">
+            <div class="m-auto py-3">
+                <i class="fas fa-file-alt icono-menu"></i>
+            </div>
             <div class="card-body">
                 <h5 class="card-title"><a href="/reporte">Reportes</a></h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
             </div>
         </div>
         <div class="card col-md-4 mb-2">
-            <img src="..." class="card-img-top" alt="...">
+            <div class="m-auto py-3">
+                <i class="fas fa-map-marker-alt icono-menu"></i>
+            </div>
             <div class="card-body">
                 <h5 class="card-title"><a href="/seguimiento">Seguimiento</a></h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
             </div>
         </div>
         <div class="card col-md-4 mb-2">
-            <img src="..." class="card-img-top" alt="...">
+            <div class="m-auto py-3">
+                <i class="fas fa-tools icono-menu"></i>
+            </div>
             <div class="card-body">
                 <h5 class="card-title"><a href="/mantenimiento">Mantenimiento</a></h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
             </div>
         </div>
-        <!--<i class="far fa-address-book"></i>-->
     </div>
 </div>
 
